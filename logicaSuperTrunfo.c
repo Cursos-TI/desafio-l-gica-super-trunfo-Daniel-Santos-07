525d7585d269d39396f21f9cf660084e9a5f6bff
Daniel-Santos-07 <dandan0795@icloud.com>
2025-08-20 21:07:26 +0000

    Nível Aventureiro

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
       char estado[50], codigo[50], nome[50];
@@ -127,85 +124,117 @@ int main(){
 
       printf("\n*** Disputa de Cartas ***\n\n");
 
-      printf("Carta %d - População: %lu \n", numero, populacao); // Exibe a população da primeira carta cadastrada para a melhor visualização da disputa.
-       printf("Carta %d - População: %lu \n", numero2, populacao2);
+      int escolha;
+
+    printf("Escolha uma opção:\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Pontos Turísticos\n");
+    printf("5. Densidade Populacional\n");
+    printf("6. PIB per Capita\n");
+    printf("7. Super Poder\n");
+    printf("Escolha: ");
+    scanf("%d", &escolha);
+
+    switch (escolha){
+
+        case 1:
+      printf("Carta %d (%s) - População: %lu \n", numero, nome, populacao); // Exibe a população da primeira carta cadastrada para a melhor visualização da disputa.
+       printf("Carta %d (%s) - População: %lu \n", numero2, nome2, populacao2);
 
        // Disputa entre as cartas com a mensagem dizendo qual carta é a vencedora, isso se repete com as demais informações.
 
       if(populacao > populacao2) {
-          printf("A carta %d - População: %.2lu venceu! \n\n", numero, populacao);
+          printf("A carta %d (%s) - População: %.2lu venceu! \n\n", numero, nome, populacao);
       } else if(populacao < populacao2) {
-          printf("A carta %d - População: %.2lu venceu! \n\n", numero2, populacao2);
+          printf("A carta %d (%s) - População: %.2lu venceu! \n\n", numero2, nome2, populacao2);
       } else {
           printf("As cartas %d e %d têm a mesma população (%lu = %lu)\n\n", numero, numero2, populacao, populacao2);
       }
+      break;
 
-      printf("Carta %d - Área: %.2f \n", numero, area); // Exibe a área da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - Área: %.2f \n", numero2, area2);
+      case 2:
+      printf("Carta %d (%s) - Área: %.2f \n", numero, nome, area); // Exibe a área da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - Área: %.2f \n", numero2, nome2, area2);
 
       if(area > area2) {
-          printf("A carta %d - Área: %.2f venceu! \n\n", numero, area);
+          printf("A carta %d (%s) - Área: %.2f venceu! \n\n", numero, nome, area);
       } else if(area < area2) {
-          printf("A carta %d - Área: %.2f venceu! \n\n", numero2, area2);
+          printf("A carta %d (%s) - Área: %.2f venceu! \n\n", numero2, nome2, area2);
       } else {
           printf("As cartas %d e %d têm a mesma área (Área: %.2f = Área: %.2f)\n\n", numero, numero2, area, area2);
       }
+      break;
 
-      printf("Carta %d - PIB: %.2f  \n", numero, PIB); // Exibe o PIB da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - PIB: %.2f  \n", numero2, PIB2);
+      case 3:
+      printf("Carta %d (%s) - PIB: %.2f  \n", numero, nome, PIB); // Exibe o PIB da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - PIB: %.2f  \n", numero2, nome2, PIB2);
 
       if(PIB > PIB2) {
-          printf("A carta %d - PIB: %.2f venceu! \n\n", numero, PIB);
+          printf("A carta %d (%s) - PIB: %.2f venceu! \n\n", numero, nome, PIB);
       } else if(PIB < PIB2) {
-          printf("A carta %d - PIB: %.2f venceu! \n\n", numero2, PIB2);
+          printf("A carta %d (%s) - PIB: %.2f venceu! \n\n", numero2, nome2, PIB2);
       } else {
           printf("As cartas %d e %d têm o mesmo PIB (PIB: %.2f = PIB: %.2f)\n\n", numero, numero2, PIB, PIB2);
+    }
+      break;
 
-      }
-
-      printf("Carta %d - Pontos Turísticos: %d \n", numero, numeropontosturisticos); // Exibe os pontos turísticos da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - Pontos Turísticos: %d \n", numero2, numeropontosturisticos2);
+      case 4:
+      printf("Carta %d (%s) - Pontos Turísticos: %d \n", numero, nome, numeropontosturisticos); // Exibe os pontos turísticos da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - Pontos Turísticos: %d \n", numero2, nome2, numeropontosturisticos2);
 
        if(numeropontosturisticos > numeropontosturisticos2) {
-          printf("A carta %d - Pontos Turísticos: %.2d venceu! \n\n", numero, numeropontosturisticos);
+          printf("A carta %d (%s) - Pontos Turísticos: %d venceu! \n\n", numero, nome, numeropontosturisticos);
       } else if(numeropontosturisticos < numeropontosturisticos2) {
-          printf("A carta %d - Pontos Turísticos: %.2d venceu! \n\n", numero2, numeropontosturisticos2);
+          printf("A carta %d (%s) - Pontos Turísticos: %d venceu! \n\n", numero2, nome2, numeropontosturisticos2);
       } else {
           printf("As cartas %d e %d têm o mesmo número de pontos turísticos (%d = %d)\n\n", numero, numero2, numeropontosturisticos, numeropontosturisticos2);
       }
+      break;
 
-      printf("Carta %d - Densidade Populacional: %.2f\n", numero, DensidadePopulacional); // Exibe a densidade populacional da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - Densidade Populacional: %.2f\n", numero2, DensidadePopulacional2);
+      case 5:
+      printf("Carta %d (%s) - Densidade Populacional: %.2f\n", numero, nome, DensidadePopulacional); // Exibe a densidade populacional da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - Densidade Populacional: %.2f\n", numero2, nome2, DensidadePopulacional2);
 
       if(DensidadePopulacional < DensidadePopulacional2) {
-          printf("A carta %d - Densidade Populacional: %.2f venceu! \n\n", numero, DensidadePopulacional);
+          printf("A carta %d (%s) - Densidade Populacional: %.2f venceu! \n\n", numero, nome, DensidadePopulacional);
       } else if(DensidadePopulacional > DensidadePopulacional2) {
-          printf("A carta %d - Densidade Populacional: %.2f venceu! \n\n", numero2, DensidadePopulacional2);
+          printf("A carta %d (%s) - Densidade Populacional: %.2f venceu! \n\n", numero2, nome2, DensidadePopulacional2);
       } else {
           printf("As cartas %d e %d têm a mesma densidade populacional (Densidade Populacional: %.2f = Densidade Populacional: %.2f)\n\n", numero, numero2, DensidadePopulacional, DensidadePopulacional2);
       }
+      break;
 
-      printf("Carta %d - PIB Per Capita: %.2f\n", numero, PIBPerCapita); // Exibe o PIB Per Capita da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - PIB Per Capita: %.2f\n", numero2, PIBPerCapita2);
+      case 6:
+      printf("Carta %d (%s) - PIB Per Capita: %.2f\n", numero, nome, PIBPerCapita); // Exibe o PIB Per Capita da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - PIB Per Capita: %.2f\n", numero2, nome2, PIBPerCapita2);
 
        if(PIBPerCapita > PIBPerCapita2) {
-          printf("A carta %d - PIB Per Capita: %.2f venceu! \n\n", numero, PIBPerCapita);
+          printf("A carta %d (%s) - PIB Per Capita: %.2f venceu! \n\n", numero, nome, PIBPerCapita);
       } else if(PIBPerCapita < PIBPerCapita2) {
-          printf("A carta %d - PIB Per Capita: %.2f venceu! \n\n", numero2, PIBPerCapita2);
+          printf("A carta %d (%s) - PIB Per Capita: %.2f venceu! \n\n", numero2, nome2, PIBPerCapita2);
       } else {
           printf("As cartas %d e %d têm o mesmo PIB Per Capita (PIB Per Capita: %.2f = PIB Per Capita: %.2f)\n\n", numero, numero2, PIBPerCapita, PIBPerCapita2);
       }
+      break;
 
-      printf("Carta %d - Super Poder: %.2f\n", numero, superPoder);  // Exibe o Super Poder da primeira carta cadastrada para a melhor visualização da disputa.
-      printf("Carta %d - Super Poder: %.2f\n", numero2, superPoder2);
+      case 7:
+      printf("Carta %d (%s) - Super Poder: %.2f\n", numero, nome, superPoder);  // Exibe o Super Poder da primeira carta cadastrada para a melhor visualização da disputa.
+      printf("Carta %d (%s) - Super Poder: %.2f\n", numero2, nome2, superPoder2);
 
        if(superPoder > superPoder2) {
-          printf("A carta %d - Super Poder: %.2f venceu! \n\n", numero, superPoder);
+          printf("A carta %d (%s) - Super Poder: %.2f venceu! \n\n", numero, nome, superPoder);
       } else if(superPoder < superPoder2) {
-          printf("A carta %d - Super Poder: %.2f venceu! \n\n", numero2, superPoder2);
+          printf("A carta %d (%s) - Super Poder: %.2f venceu! \n\n", numero2, nome2, superPoder2);
       } else {
           printf("As cartas %d e %d têm o mesmo poder (Super Poder: %.2f = Super Poder: %.2f)\n\n", numero, numero2, superPoder, superPoder2);
       }
+      break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
 
       return 0;
 
